5bd7fc68090dca85ca165ddf0f813da44ab13b01
Aldair LUNA BUITRON <aldairl@unicauca.edu.co>
2021-05-06 23:20:13 -0500

    new method mercado pago

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var port = process.env.PORT || 3000
+const bodyParser = require('body-parser');
+
+const mercadoPago = require('./controller/mercadopago');
 
 var app = express();
- 
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 app.use(express.static('assets'));
- 
+
 app.use('/assets', express.static(__dirname + '/assets'));
 
 app.get('/', function (req, res) {
@@ -18,5 +26,30 @@ app.get('/', function (req, res) {
 app.get('/detail', function (req, res) {
     res.render('detail', req.query);
 });
+
+app.post('/checkout', mercadoPago.creteReference);
+
+app.get('/success', (req, res) => {
+    console.log(req.query);
+    console.log(req.body, "body");
+    res.render('success', req.query)
+});
+
+app.get('/failure', (req, res) => {
+    console.log(req.query);
+    console.log(req.body, "body");
+    res.render('failure', req.query);
+});
+
+app.get('/pending', (req, res) => {
+    console.log(req.query);
+    console.log(req.body, "body");
+    res.render('pending', req.query);
+});
+
+app.post('/webhook', (req, res) => {
+    console.log(req.body, "respuestas");
+    res.status(200).send('ok');
+})
 
 app.listen(port);
